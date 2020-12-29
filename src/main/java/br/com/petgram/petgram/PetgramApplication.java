e1068fb5f4ffed72d9392aeb389ed5c1d174c360
Gabriel Pickler <gabrielpickler@pop-os.localdomain>
2020-12-29 20:39:52 -0300

    projeto iniciado

---
+++ src/main/java/br/com/petgram/petgram/PetgramApplication.java
@@ -0,0 +1,13 @@
+package br.com.petgram.petgram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetgramApplication.class, args);
+	}
+
+}
